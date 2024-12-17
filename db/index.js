ecc05f3bfacd89ca686e33c0908adafd39f59586
BennettMa23 <19906406473@163.com>
2024-12-17 17:43:32 +0800

    ok

---
+++ db/index.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+  host: 'localhost',
+  username: 'novel',
+  password: '123123',
+  database: 'novel',
+  dialect: 'mysql',
+  define: {
+    charset: 'utf8mb4'
+  },
+  // dialectOptions: {
+  //   collate: 'utf8mb4_unicode_ci'
+  // }
+})
+
+// 测试连接
+sequelize.authenticate().then(_=>{
+  console.log("数据库连接成功!");
+}).catch(err=>{
+  console.log("数据库连接失败! ", err);
+})
+
+sequelize.sync();
+
+
+module.exports = sequelize
